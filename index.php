f0e2969d4543fc20f330fbb374419c906b67a130
Samuel Schwab <sam2schwab@gmail.com>
2016-11-02 15:26:03 -0400

    interface criteres de recherche cree en place mais non fonctionnel

--- index.php
+++ index.php
@@ -11,56 +11,54 @@ $categories = ["Entrée","Repas","Dessert","Pain","Conseil","Sauce","Boisson"];
 	</head>
 	<body>
 		<?php include("header.php");?>
-		<div class="container">
-			<article>
-				<div class="col-md-8">
-					<!--Recherche par mot-cles-->
-					<div class="panel panel-default">
-						<div class="panel-heading">Recherche par mots-clés</div>
-						<div class="panel-body">
-							<div class="form-group">
-								<label for="keyword-search">Mots-clés:</label>
-								<div class="input-group" id="keyword-search">
-									<input type="text" class="form-control">
-									<span class="input-group-btn">
-										<button type="submit" class="btn btn-primary btn-block">
-											<span class="glyphicon glyphicon-search"></span> Recherche
-										</button>
-									</span>
-								</div>
+		<article class="container">
+			<div class="col-md-8">
+				<!--Recherche par mot-cles-->
+				<div class="panel panel-default">
+					<div class="panel-heading">Recherche par mots-clés</div>
+					<div class="panel-body">
+						<div class="form-group">
+							<label for="keyword-search">Mots-clés:</label>
+							<div class="input-group" id="keyword-search">
+								<input type="text" class="form-control">
+								<span class="input-group-btn">
+									<button type="submit" class="btn btn-primary btn-block">
+										<span class="glyphicon glyphicon-search"></span> Recherche
+									</button>
+								</span>
 							</div>
-						</div>
-					</div>
-					<!--Recherche par ingredients-->
-					<div class="panel panel-default">
-						<div class="panel-heading">Recherche par ingrédients</div>
-						<div class="panel-body">
-							<div class="form-group">
-								<label for="ingredients-with">Avec:</label>			
-								<input type="text" class="form-control" data-role="tagsinput" id="ingredients-with">
-							</div>
-							<div class="form-group">
-								<label for="ingredients-without">Sans:</label>			
-								<input type="text" class="form-control" data-role="tagsinput" id="ingredients-without">
-							</div>
-							<button type="submit" class="btn btn-primary pull-right">
-								<span class="glyphicon glyphicon-search"></span> Recherche
-							</button>
 						</div>
 					</div>
 				</div>
-				<div class="col-md-4">
-					<!--Recherche par catégories-->
-					<div class="panel panel-default">
-						<div class="panel-heading">Catégories</div>
-						<div class="panel-body">
-							<?php foreach ($categories as $value) {
-								echo '<button class="btn btn-lg btn-default btn-block">'.$value.'</button>';
-							}?>
+				<!--Recherche par ingredients-->
+				<div class="panel panel-default">
+					<div class="panel-heading">Recherche par ingrédients</div>
+					<div class="panel-body">
+						<div class="form-group">
+							<label for="ingredients-with">Avec:</label>			
+							<input type="text" class="form-control" data-role="tagsinput" id="ingredients-with">
 						</div>
+						<div class="form-group">
+							<label for="ingredients-without">Sans:</label>			
+							<input type="text" class="form-control" data-role="tagsinput" id="ingredients-without">
+						</div>
+						<button type="submit" class="btn btn-primary pull-right">
+							<span class="glyphicon glyphicon-search"></span> Recherche
+						</button>
 					</div>
 				</div>
-			</article>
-		</div>
+			</div>
+			<div class="col-md-4">
+				<!--Recherche par catégories-->
+				<div class="panel panel-default">
+					<div class="panel-heading">Catégories</div>
+					<div class="panel-body">
+						<?php foreach ($categories as $value) {
+							echo '<button class="btn btn-lg btn-default btn-block">'.$value.'</button>';
+						}?>
+					</div>
+				</div>
+			</div>
+		</article>
 	</body>
 </html>
--- recherche.php
+++ recherche.php
@@ -1,4 +1,6 @@
-<?php $page = "recherche"?>
+<?php $page = "recherche";
+$categories = ["Entrée","Repas","Dessert","Pain","Conseil","Sauce","Boisson"];
+?>
 
 <!DOCTYPE html>
 <html>
@@ -9,5 +11,82 @@
 	</head>
 	<body>
     	<?php include("header.php");?>
+		<article class="container">
+			<div class="col-xs-3">
+				<div class="panel panel-default">
+					<div class="panel-heading">Critères de recherche</div>
+					<div class="panel-body">
+						<!--Mot-cles-->
+						<div>
+							<button type="button" class="close">&times;</button>
+							<h4><strong>Mots-clés</strong></h4>
+							<input type="text" class="form-control" data-role="tagsinput" id="ingredients-with">
+						</div>
+						<hr>
+						<!--ingredients-->
+						<div>
+							<button type="button" class="close">&times;</button>
+							<h4><strong>Ingrédients</strong></h4>
+							<div class="form-group">
+								<label for="ingredients-with">Avec:</label>			
+								<input type="text" class="form-control" data-role="tagsinput" id="ingredients-with">
+							</div>
+							<div class="form-group">
+								<label for="ingredients-without">Sans:</label>			
+								<input type="text" class="form-control" data-role="tagsinput" id="ingredients-without">
+							</div>
+						</div>
+						<hr>
+						<!--cuisson-->
+						<div>
+							<button type="button" class="close">&times;</button>
+							<h4><strong>Cuisson</strong></h4>
+							<div class="radio">
+								<label><input type="radio" name="cookingradio">Avec</label>
+							</div>
+							<div class="radio">
+								<label><input type="radio" name="cookingradio">Sans</label>
+							</div>
+						</div>
+						<hr>
+						<!--temps-->
+						<div>
+							<button type="button" class="close">&times;</button>
+							<h4><strong>Temps requis</strong></h4>
+							<div class="form-group">
+								<label for="prep-time">Préparation</label>
+								<div class="input-group" id="prep-time">
+									<span class="input-group-addon">&#8804;</span>
+									<input type="number" min="0" data-bind="value:replyNumber" class="form-control">
+									<span class="input-group-addon">mn</span>
+								</div>
+							</div>
+							<div class="form-group">
+								<label for="cook-time">Cuisson</label>
+								<div class="input-group" id="cook-time">
+									<span class="input-group-addon">&#8804;</span>
+									<input type="number" min="0" class="form-control">
+									<span class="input-group-addon">mn</span>
+								</div>
+							</div>
+						</div>
+						<hr>
+						<!--categorie-->
+						<div>
+							<button type="button" class="close">&times;</button>
+							<h4><strong>Catégories</strong></h4>
+							<?php foreach ($categories as $value) {
+							echo '<div class="checkbox">
+								<label><input type="checkbox" value="">'.$value.'</label>
+							</div>';
+							}?>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-xs-9">
+				
+			</div>			
+		</article>
 	</body>
 </html>
